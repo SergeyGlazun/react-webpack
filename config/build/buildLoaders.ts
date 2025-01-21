da8799e81dc9cefdc69955bc81d1f485879304c0
u_m1pbp <SGlazunov@alfabank.ru>
2025-01-21 23:37:34 +0300

    Разрешены конфликты

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,31 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import { BuildOptions } from "./types/config";
+
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resourcePath: string) => {
+              return resourcePath.endsWith('.module.scss');
+            },
+          }
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  return [typescriptLoader, cssLoader];
+}
